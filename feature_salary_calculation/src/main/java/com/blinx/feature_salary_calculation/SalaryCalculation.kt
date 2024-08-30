825bcf9e7618baff1cd3fb48b44824c1605265e9
jido2015 <bashorunolajide@gmail.com>
2024-08-29 20:33:54 -0400

    Init

---
+++ feature_salary_calculation/src/main/java/com/blinx/feature_salary_calculation/SalaryCalculation.kt
@@ -0,0 +1,7 @@
+package com.blinx.feature_salary_calculation
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun SalaryCalculation() {
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SalaryMate"
+include(":app")
+include(":core")
+include(":feature_authentication")
+include(":feature_salary_calculation")
+include(":network")
+include(":data")
